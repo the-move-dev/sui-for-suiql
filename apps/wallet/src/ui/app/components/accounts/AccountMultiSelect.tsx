asonnino/sui

update dapp connect accounts selector

apps/wallet/src/ui/app/components/accounts/AccountMultiSelect.tsx
@@ -3,13 +3,16 @@
 
 import * as ToggleGroup from '@radix-ui/react-toggle-group';
 
+import { useState } from 'react';
 import { AccountMultiSelectItem } from './AccountMultiSelectItem';
+import { Button } from '../../shared/ButtonUI';
 import { type SerializedUIAccount } from '_src/background/accounts/Account';
 
 type AccountMultiSelectProps = {
 	accounts: SerializedUIAccount[];
 	selectedAccountIDs: string[];
 	onChange: (value: string[]) => void;
+	enableSelectAll?: boolean;
 };
 
 export function AccountMultiSelect({
@@ -32,5 +35,43 @@ export function AccountMultiSelect({
 				/>
 			))}
 		</ToggleGroup.Root>
+	);
+}
+
+export function AccountMultiSelectWithControls({
+	selectedAccounts: selectedAccountsFromProps,
+	accounts,
+	onChange: onChangeFromProps,
+}: AccountMultiSelectProps) {
+	const [selectedAccounts, setSelectedAccounts] = useState(selectedAccountsFromProps);
+	const onChange = (value: string[]) => {
+		setSelectedAccounts(value);
+		onChangeFromProps(value);
+	};
+	return (
+		<div className="flex flex-col gap-3 [&>button]:border-none">
+			<AccountMultiSelect
+				selectedAccounts={selectedAccounts}
+				accounts={accounts}
+				onChange={onChange}
+			/>
+
+			<Button
+				onClick={() => {
+					if (selectedAccounts.length < accounts.length) {
+						onChange(accounts.map((account) => account.address));
+					} else {
+						onChange([]);
+					}
+				}}
+				variant="outline"
+				size="xs"
+				text={
+					selectedAccounts.length < accounts.length
+						? 'Select All Accounts'
+						: 'Deselect All Accounts'
+				}
+			/>
+		</div>
 	);
 }

apps/wallet/src/ui/app/pages/site-connect/index.tsx
@@ -4,16 +4,19 @@
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { DAppPermissionsList } from '../../components/DAppPermissionsList';
-import { SummaryCard } from '../../components/SummaryCard';
-import { WalletListSelect } from '../../components/WalletListSelect';
+import { AccountMultiSelectWithControls } from '../../components/accounts/AccountMultiSelect';
+import { useAccounts } from '../../hooks/useAccounts';
+import { useActiveAccount } from '../../hooks/useActiveAccount';
+
 import { PageMainLayoutTitle } from '../../shared/page-main-layout/PageMainLayoutTitle';
+
 import Loading from '_components/loading';
 import { UserApproveContainer } from '_components/user-approve-container';
-import { useActiveAddress, useAppDispatch, useAppSelector } from '_hooks';
+import { useAppDispatch, useAppSelector } from '_hooks';
 import { permissionsSelectors, respondToPermissionRequest } from '_redux/slices/permissions';
-import { ampli } from '_src/shared/analytics/ampli';
 
+import { type SerializedAccount } from '_src/background/keyring/Account';
+import { ampli } from '_src/shared/analytics/ampli';
 import type { RootState } from '_redux/RootReducer';
 
 import st from './SiteConnectPage.module.scss';
@@ -29,9 +32,10 @@ function SiteConnectPage() {
 	);
 	const dispatch = useAppDispatch();
 	const permissionRequest = useAppSelector(permissionSelector);
-	const activeAddress = useActiveAddress();
-	const [accountsToConnect, setAccountsToConnect] = useState<string[]>(() =>
-		activeAddress ? [activeAddress] : [],
+	const activeAccount = useActiveAccount();
+	const accounts = useAccounts();
+	const [accountsToConnect, setAccountsToConnect] = useState<SerializedAccount[]>(() =>
+		activeAccount ? [activeAccount] : [],
 	);
 	const handleOnSubmit = useCallback(
 		async (allowed: boolean) => {
@@ -39,7 +43,7 @@ function SiteConnectPage() {
 				await dispatch(
 					respondToPermissionRequest({
 						id: requestID,
-						accounts: allowed ? accountsToConnect : [],
+						accounts: allowed ? accountsToConnect.map((account) => account.address) : [],
 						allowed,
 					}),
 				);
@@ -112,6 +116,7 @@ function SiteConnectPage() {
 					<UserApproveContainer
 						origin={permissionRequest.origin}
 						originFavIcon={permissionRequest.favIcon}
+						permissions={permissionRequest.permissions}
 						approveTitle="Connect"
 						rejectTitle="Reject"
 						onSubmit={handleOnSubmit}
@@ -119,16 +124,15 @@ function SiteConnectPage() {
 						blended
 					>
 						<PageMainLayoutTitle title="Approve Connection" />
-						<SummaryCard
-							header="Permissions requested"
-							body={<DAppPermissionsList permissions={permissionRequest.permissions} />}
-							boxShadow
-						/>
-						<WalletListSelect
-							title="Connect Accounts"
-							values={accountsToConnect}
-							onChange={setAccountsToConnect}
-							boxShadow
+
+						<AccountMultiSelectWithControls
+							selectedAccounts={accountsToConnect.map((account) => account.address)}
+							accounts={accounts}
+							onChange={(value) => {
+								setAccountsToConnect(
+									value.map((address) => accounts.find((a) => a.address === address)!),
+								);
+							}}
 						/>
 					</UserApproveContainer>
 				))}

the-move-dev/sui-for-suiql

apps/wallet/src/ui/app/components/accounts/AccountMultiSelect.tsx
@@ -3,13 +3,16 @@
 
 import * as ToggleGroup from '@radix-ui/react-toggle-group';
 
+import { useState } from 'react';
 import { AccountMultiSelectItem } from './AccountMultiSelectItem';
+import { Button } from '../../shared/ButtonUI';
 import { type SerializedUIAccount } from '_src/background/accounts/Account';
 
 type AccountMultiSelectProps = {
 	accounts: SerializedUIAccount[];
 	selectedAccountIDs: string[];
 	onChange: (value: string[]) => void;
+	enableSelectAll?: boolean;
 };
 
 export function AccountMultiSelect({
@@ -32,5 +35,43 @@ export function AccountMultiSelect({
 				/>
 			))}
 		</ToggleGroup.Root>
+	);
+}
+
+export function AccountMultiSelectWithControls({
+	selectedAccountIDs: selectedAccountsFromProps,
+	accounts,
+	onChange: onChangeFromProps,
+}: AccountMultiSelectProps) {
+	const [selectedAccounts, setSelectedAccounts] = useState(selectedAccountsFromProps);
+	const onChange = (value: string[]) => {
+		setSelectedAccounts(value);
+		onChangeFromProps(value);
+	};
+	return (
+		<div className="flex flex-col gap-3 [&>button]:border-none">
+			<AccountMultiSelect
+				selectedAccountIDs={selectedAccounts}
+				accounts={accounts}
+				onChange={onChange}
+			/>
+
+			<Button
+				onClick={() => {
+					if (selectedAccounts.length < accounts.length) {
+						onChange(accounts.map((account) => account.address));
+					} else {
+						onChange([]);
+					}
+				}}
+				variant="outline"
+				size="xs"
+				text={
+					selectedAccounts.length < accounts.length
+						? 'Select All Accounts'
+						: 'Deselect All Accounts'
+				}
+			/>
+		</div>
 	);
 }

apps/wallet/src/ui/app/pages/site-connect/index.tsx
@@ -4,16 +4,19 @@
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { DAppPermissionsList } from '../../components/DAppPermissionsList';
-import { SummaryCard } from '../../components/SummaryCard';
-import { WalletListSelect } from '../../components/WalletListSelect';
+import { AccountMultiSelectWithControls } from '../../components/accounts/AccountMultiSelect';
+import { useAccounts } from '../../hooks/useAccounts';
+import { useActiveAccount } from '../../hooks/useActiveAccount';
+
 import { PageMainLayoutTitle } from '../../shared/page-main-layout/PageMainLayoutTitle';
+
 import Loading from '_components/loading';
 import { UserApproveContainer } from '_components/user-approve-container';
-import { useActiveAddress, useAppDispatch, useAppSelector } from '_hooks';
+import { useAppDispatch, useAppSelector } from '_hooks';
 import { permissionsSelectors, respondToPermissionRequest } from '_redux/slices/permissions';
-import { ampli } from '_src/shared/analytics/ampli';
 
+import { type SerializedUIAccount } from '_src/background/accounts/Account';
+import { ampli } from '_src/shared/analytics/ampli';
 import type { RootState } from '_redux/RootReducer';
 
 import st from './SiteConnectPage.module.scss';
@@ -29,9 +32,10 @@ function SiteConnectPage() {
 	);
 	const dispatch = useAppDispatch();
 	const permissionRequest = useAppSelector(permissionSelector);
-	const activeAddress = useActiveAddress();
-	const [accountsToConnect, setAccountsToConnect] = useState<string[]>(() =>
-		activeAddress ? [activeAddress] : [],
+	const activeAccount = useActiveAccount();
+	const { data: accounts } = useAccounts();
+	const [accountsToConnect, setAccountsToConnect] = useState<SerializedUIAccount[]>(() =>
+		activeAccount ? [activeAccount] : [],
 	);
 	const handleOnSubmit = useCallback(
 		async (allowed: boolean) => {
@@ -39,7 +43,7 @@ function SiteConnectPage() {
 				await dispatch(
 					respondToPermissionRequest({
 						id: requestID,
-						accounts: allowed ? accountsToConnect : [],
+						accounts: allowed ? accountsToConnect.map((account) => account.address) : [],
 						allowed,
 					}),
 				);
@@ -112,6 +116,7 @@ function SiteConnectPage() {
 					<UserApproveContainer
 						origin={permissionRequest.origin}
 						originFavIcon={permissionRequest.favIcon}
+						permissions={permissionRequest.permissions}
 						approveTitle="Connect"
 						rejectTitle="Reject"
 						onSubmit={handleOnSubmit}
@@ -119,16 +124,15 @@ function SiteConnectPage() {
 						blended
 					>
 						<PageMainLayoutTitle title="Approve Connection" />
-						<SummaryCard
-							header="Permissions requested"
-							body={<DAppPermissionsList permissions={permissionRequest.permissions} />}
-							boxShadow
-						/>
-						<WalletListSelect
-							title="Connect Accounts"
-							values={accountsToConnect}
-							onChange={setAccountsToConnect}
-							boxShadow
+
+						<AccountMultiSelectWithControls
+							selectedAccountIDs={accountsToConnect.map((account) => account.address)}
+							accounts={accounts ?? []}
+							onChange={(value) => {
+								setAccountsToConnect(
+									value.map((address) => accounts?.find((a) => a.address === address)!),
+								);
+							}}
 						/>
 					</UserApproveContainer>
 				))}