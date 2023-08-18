RevanthGundala/sui

[tto-sdks] Add typescript SDK support and plumb out into json-rpc

sdk/typescript/src/builder/TransactionBlock.ts
@@ -86,6 +86,18 @@ function createTransactionResult(index: number): TransactionResult {
 	}) as TransactionResult;
 }
 
+function isReceivingType(normalizedType: SuiMoveNormalizedType): boolean {
+	const tag = extractStructTag(normalizedType);
+	if (tag) {
+		return (
+			tag.Struct.address === '0x2' &&
+			tag.Struct.module === 'transfer' &&
+			tag.Struct.name === 'Receiving'
+		);
+	}
+	return false;
+}
+
 function expectClient(options: BuildOptions): JsonRpcProvider | SuiClient {
 	if (!options.client && !options.provider) {
 		throw new Error(
@@ -284,11 +296,31 @@ export class TransactionBlock {
 	}
 
 	/**
+	 * Add a new object input to the transaction.
+	 */
+	receiving(value: ObjectId | ObjectCallArg) {
+		const id = getIdFromCallArg(value);
+		// deduplicate
+		const inserted = this.#blockData.inputs.find(
+			(i) => i.type === 'object' && id === getIdFromCallArg(i.value),
+		);
+		return inserted ?? this.#input('object', value);
+	}
+
+	/**
 	 * Add a new object input to the transaction using the fully-resolved object reference.
 	 * If you only have an object ID, use `builder.object(id)` instead.
 	 */
 	objectRef(...args: Parameters<(typeof Inputs)['ObjectRef']>) {
 		return this.object(Inputs.ObjectRef(...args));
+	}
+
+	/**
+	 * Add a new receiving input to the transaction using the fully-resolved object reference.
+	 * If you only have an object ID, use `builder.receiving(id)` instead.
+	 */
+	receivingRef(...args: Parameters<(typeof Inputs)['ReceivingRef']>) {
+		return this.receiving(Inputs.ReceivingRef(...args));
 	}
 
 	/**
@@ -684,6 +716,8 @@ export class TransactionBlock {
 						initialSharedVersion,
 						mutable,
 					});
+				} else if (normalizedType && isReceivingType(normalizedType)) {
+					input.value = Inputs.ReceivingRef(getObjectReference(object)!);
 				} else {
 					input.value = Inputs.ObjectRef(getObjectReference(object)!);
 				}

sdk/typescript/src/builder/__tests__/Transaction.test.ts
@@ -11,6 +11,23 @@ it('can construct and serialize an empty tranaction', () => {
 	expect(() => tx.serialize()).not.toThrow();
 });
 
+it('can construct a receiving transaction argument', () => {
+	const tx = new TransactionBlock();
+	tx.receiving(Inputs.ReceivingRef(ref()));
+	expect(() => tx.serialize()).not.toThrow();
+});
+
+it('receiving transaction argument different from object argument', () => {
+	const oref = ref();
+	const rtx = new TransactionBlock();
+	rtx.receiving(Inputs.ReceivingRef(oref));
+	const otx = new TransactionBlock();
+	otx.receiving(Inputs.ObjectRef(oref));
+	expect(() => rtx.serialize()).not.toThrow();
+	expect(() => otx.serialize()).not.toThrow();
+	expect(otx.serialize()).not.toEqual(rtx.serialize());
+});
+
 it('can be serialized and deserialized to the same values', () => {
 	const tx = new TransactionBlock();
 	tx.add(Transactions.SplitCoins(tx.gas, [tx.pure(100)]));
@@ -106,6 +123,26 @@ describe('offline build', () => {
 			}),
 		);
 		await tx.build();
+	});
+
+	it('uses a receiving argument', async () => {
+		const tx = setup();
+		tx.receiving(Inputs.ObjectRef(ref()));
+		const coin = tx.add(Transactions.SplitCoins(tx.gas, [tx.pure(100)]));
+		tx.add(Transactions.MergeCoins(tx.gas, [coin, tx.object(Inputs.ObjectRef(ref()))]));
+		tx.add(
+			Transactions.MoveCall({
+				target: '0x2::devnet_nft::mint',
+				typeArguments: [],
+				arguments: [tx.object(Inputs.ObjectRef(ref())), tx.receiving(Inputs.ReceivingRef(ref()))],
+			}),
+		);
+
+		const bytes = await tx.build();
+		const tx2 = TransactionBlock.from(bytes);
+		const bytes2 = await tx2.build();
+
+		expect(bytes).toEqual(bytes2);
 	});
 
 	it('builds a more complex interaction', async () => {

the-move-dev/sui-for-suiql

sdk/typescript/src/builder/TransactionBlock.ts
@@ -86,6 +86,18 @@ function createTransactionResult(index: number): TransactionResult {
 	}) as TransactionResult;
 }
 
+function isReceivingType(normalizedType: SuiMoveNormalizedType): boolean {
+	const tag = extractStructTag(normalizedType);
+	if (tag) {
+		return (
+			tag.Struct.address === '0x2' &&
+			tag.Struct.module === 'transfer' &&
+			tag.Struct.name === 'Receiving'
+		);
+	}
+	return false;
+}
+
 function expectClient(options: BuildOptions): SuiClient {
 	if (!options.client && !options.provider) {
 		throw new Error(
@@ -299,6 +311,14 @@ export class TransactionBlock {
 	 */
 	objectRef(...args: Parameters<(typeof Inputs)['ObjectRef']>) {
 		return this.object(Inputs.ObjectRef(...args));
+	}
+
+	/**
+	 * Add a new receiving input to the transaction using the fully-resolved object reference.
+	 * If you only have an object ID, use `builder.object(id)` instead.
+	 */
+	receivingRef(...args: Parameters<(typeof Inputs)['ReceivingRef']>) {
+		return this.object(Inputs.ReceivingRef(...args));
 	}
 
 	/**
@@ -705,6 +725,8 @@ export class TransactionBlock {
 						initialSharedVersion,
 						mutable,
 					});
+				} else if (normalizedType && isReceivingType(normalizedType)) {
+					input.value = Inputs.ReceivingRef(getObjectReference(object)!);
 				} else {
 					input.value = Inputs.ObjectRef(getObjectReference(object as SuiObjectResponse)!);
 				}

sdk/typescript/src/builder/__tests__/Transaction.test.ts
@@ -11,6 +11,23 @@ it('can construct and serialize an empty tranaction', () => {
 	expect(() => tx.serialize()).not.toThrow();
 });
 
+it('can construct a receiving transaction argument', () => {
+	const tx = new TransactionBlock();
+	tx.object(Inputs.ReceivingRef(ref()));
+	expect(() => tx.serialize()).not.toThrow();
+});
+
+it('receiving transaction argument different from object argument', () => {
+	const oref = ref();
+	const rtx = new TransactionBlock();
+	rtx.object(Inputs.ReceivingRef(oref));
+	const otx = new TransactionBlock();
+	otx.object(Inputs.ObjectRef(oref));
+	expect(() => rtx.serialize()).not.toThrow();
+	expect(() => otx.serialize()).not.toThrow();
+	expect(otx.serialize()).not.toEqual(rtx.serialize());
+});
+
 it('can be serialized and deserialized to the same values', () => {
 	const tx = new TransactionBlock();
 	tx.add(Transactions.SplitCoins(tx.gas, [tx.pure(100)]));
@@ -106,6 +123,26 @@ describe('offline build', () => {
 			}),
 		);
 		await tx.build();
+	});
+
+	it('uses a receiving argument', async () => {
+		const tx = setup();
+		tx.object(Inputs.ObjectRef(ref()));
+		const coin = tx.add(Transactions.SplitCoins(tx.gas, [tx.pure(100)]));
+		tx.add(Transactions.MergeCoins(tx.gas, [coin, tx.object(Inputs.ObjectRef(ref()))]));
+		tx.add(
+			Transactions.MoveCall({
+				target: '0x2::devnet_nft::mint',
+				typeArguments: [],
+				arguments: [tx.object(Inputs.ObjectRef(ref())), tx.object(Inputs.ReceivingRef(ref()))],
+			}),
+		);
+
+		const bytes = await tx.build();
+		const tx2 = TransactionBlock.from(bytes);
+		const bytes2 = await tx2.build();
+
+		expect(bytes).toEqual(bytes2);
 	});
 
 	it('builds a more complex interaction', async () => {