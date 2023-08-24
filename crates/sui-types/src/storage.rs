MystenLabs/sui

sui-execution generate primitive results

crates/sui-types/src/storage.rs
@@ -8,7 +8,7 @@ use crate::digests::{
 };
 use crate::effects::{TransactionEffects, TransactionEvents};
 use crate::error::SuiError;
-use crate::execution::LoadedChildObjectMetadata;
+use crate::execution::{ExecutionResultsV2, LoadedChildObjectMetadata};
 use crate::message_envelope::Message;
 use crate::messages_checkpoint::{
     CheckpointContents, CheckpointSequenceNumber, FullCheckpointContents, VerifiedCheckpoint,
@@ -130,6 +130,8 @@ pub trait Storage {
     fn read_object(&self, id: &ObjectID) -> Option<&Object>;
 
     fn apply_object_changes(&mut self, changes: BTreeMap<ObjectID, ObjectChange>);
+
+    fn record_execution_results_v2(&mut self, results: ExecutionResultsV2);
 
     fn save_loaded_child_objects(
         &mut self,

sui-execution/latest/sui-adapter/src/programmable_transactions/execution.rs
@@ -38,10 +38,8 @@ mod checked {
         },
         coin::Coin,
         error::{command_argument_error, ExecutionError, ExecutionErrorKind},
-        event::Event,
         execution::{
-            CommandKind, ExecutionResults, ExecutionState, ObjectContents, ObjectValue,
-            RawValueType, Value,
+            CommandKind, ExecutionState, ObjectContents, ObjectValue, RawValueType, Value,
         },
         gas::GasCharger,
         id::{RESOLVED_SUI_ID, UID},
@@ -107,21 +105,7 @@ mod checked {
         let finished = context.finish::<Mode>();
         // Save loaded objects for debug. We dont want to lose the info
         state_view.save_loaded_child_objects(loaded_child_objects);
-
-        let ExecutionResults {
-            object_changes,
-            user_events,
-        } = finished?;
-        state_view.apply_object_changes(object_changes);
-        for (module_id, tag, contents) in user_events {
-            state_view.log_event(Event::new(
-                module_id.address(),
-                module_id.name(),
-                tx_context.sender(),
-                tag,
-                contents,
-            ))
-        }
+        state_view.record_execution_results_v2(finished?);
         Ok(mode_results)
     }
 

the-move-dev/sui-for-suiql

crates/sui-types/src/storage.rs
@@ -8,7 +8,7 @@ use crate::digests::{
 };
 use crate::effects::{TransactionEffects, TransactionEvents};
 use crate::error::SuiError;
-use crate::execution::LoadedChildObjectMetadata;
+use crate::execution::{ExecutionResults, LoadedChildObjectMetadata};
 use crate::message_envelope::Message;
 use crate::messages_checkpoint::{
     CheckpointContents, CheckpointSequenceNumber, FullCheckpointContents, VerifiedCheckpoint,
@@ -19,7 +19,6 @@ use crate::transaction::{SenderSignedData, TransactionDataAPI, VerifiedTransacti
 use crate::{
     base_types::{ObjectID, ObjectRef, SequenceNumber},
     error::SuiResult,
-    event::Event,
     object::Object,
 };
 use itertools::Itertools;
@@ -125,12 +124,9 @@ pub trait ChildObjectResolver {
 pub trait Storage {
     fn reset(&mut self);
 
-    /// Record an event that happened during execution
-    fn log_event(&mut self, event: Event);
-
     fn read_object(&self, id: &ObjectID) -> Option<&Object>;
 
-    fn apply_object_changes(&mut self, changes: BTreeMap<ObjectID, ObjectChange>);
+    fn record_execution_results(&mut self, results: ExecutionResults);
 
     fn save_loaded_child_objects(
         &mut self,

sui-execution/latest/sui-adapter/src/programmable_transactions/execution.rs
@@ -38,10 +38,8 @@ mod checked {
         },
         coin::Coin,
         error::{command_argument_error, ExecutionError, ExecutionErrorKind},
-        event::Event,
         execution::{
-            CommandKind, ExecutionResults, ExecutionState, ObjectContents, ObjectValue,
-            RawValueType, Value,
+            CommandKind, ExecutionState, ObjectContents, ObjectValue, RawValueType, Value,
         },
         id::{RESOLVED_SUI_ID, UID},
         metrics::LimitsMetrics,
@@ -106,21 +104,7 @@ mod checked {
         let finished = context.finish::<Mode>();
         // Save loaded objects for debug. We dont want to lose the info
         state_view.save_loaded_child_objects(loaded_child_objects);
-
-        let ExecutionResults {
-            object_changes,
-            user_events,
-        } = finished?;
-        state_view.apply_object_changes(object_changes);
-        for (module_id, tag, contents) in user_events {
-            state_view.log_event(Event::new(
-                module_id.address(),
-                module_id.name(),
-                tx_context.sender(),
-                tag,
-                contents,
-            ))
-        }
+        state_view.record_execution_results(finished?);
         Ok(mode_results)
     }
 